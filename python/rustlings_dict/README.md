8b07b828e2f8b2fe50f825ca7a29e7fece0e1596
Psyhackological <konradkon@duck.com>
2023-02-02 14:57:54 +0100

    Added first Python script - Rustlings JSON Converter

---
+++ python/rustlings_dict/README.md
@@ -0,0 +1,3 @@
+# Rustlings JSON Converter
+
+This code contains a main function that converts the `exercises.txt` file to a JSON file. The `exercises.txt` file contains information about different categories and their corresponding files.
---
+++ python/rustlings_dict/rustlings_json_converter.py
@@ -0,0 +1,34 @@
+"""
+Contains main function only
+"""
+from collections import defaultdict
+import pprint
+import json
+
+
+def main():
+    """Main function that converts the "exercises.txt" file to a JSON file.
+    The "exercises.txt" file contains information
+    about different categories and their corresponding files.
+    This function reads the file, converts the information into a dictionary,
+    and writes it to a JSON file.
+    """
+    rustlings_dict = defaultdict(list)
+
+    with open("exercises.txt", "rt", encoding="utf-8") as rustlings_exercises:
+        for line in rustlings_exercises:
+            category, *file = line.strip().split(r"/")
+            rustlings_dict[category if r"/" in line else "quiz"].append(
+                file[0] if file else line.strip()
+            )
+
+    pretty_printer = pprint.PrettyPrinter(width=88, compact=True)
+    pretty_printer.pprint((dict(rustlings_dict)))
+
+    # writing the dictionary to a JSON file
+    with open("rustlings.json", "wt", encoding="utf-8") as rustlings_json:
+        json.dump(dict(rustlings_dict), rustlings_json, indent=4)
+
+
+if __name__ == "__main__":
+    main()
